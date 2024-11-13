d8f8bac95e853011fe0f144e88e86b3fb12a596c
Jeffrey Davis <itechify@gmail.com>
2024-11-13 04:19:01 -0800

    use auth for homepage

--- README.md
+++ README.md
@@ -39,6 +39,7 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 - [x] Attach database to ui
 - [x] Add authentication (w/ clerk)
 - [ ] Add image upload
+- [ ] "taint" (server-only)
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parrallel route)
 - [ ] Delete button (w/ Server Actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,51 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Boulders() {
   const boulders = await db.query.boulders.findMany();
 
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4 px-4">
-        {[...boulders].map((boulder, index) => (
-          <div key={index} className="flex flex-col gap-2">
-            <div className="flex gap-2">
-              <div className="h-16 w-16 rounded-lg bg-gray-200"></div>
-              <div className="flex flex-col gap-1">
-                <div className="text-xl font-bold">{boulder.name}</div>
-                <div className="text-sm text-gray-500">
-                  {boulder.grade} / {boulder.rating} stars
-                </div>
+    <div className="flex flex-wrap gap-4 px-4">
+      {[...boulders].map((boulder, index) => (
+        <div key={index} className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <div className="h-16 w-16 rounded-lg bg-gray-200"></div>
+            <div className="flex flex-col gap-1">
+              <div className="text-xl font-bold">{boulder.name}</div>
+              <div className="text-sm text-gray-500">
+                {boulder.grade} / {boulder.rating} stars
               </div>
             </div>
-            <div className="flex gap-2">
-              <a
-                href={boulder.url}
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                {boulder.url}
-              </a>
-            </div>
           </div>
-        ))}
-      </div>
+          <div className="flex gap-2">
+            <a
+              href={boulder.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              {boulder.url}
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Boulders />
+      </SignedIn>
     </main>
   );
 }
